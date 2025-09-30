d34ee06555be685e6f26897b470a5bae4371b2a0
Alberto Ielpo <alberto.ielpo@gmail.com>
2025-09-30 15:05:45 +0200

    Doc: comments

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+/**
+ * @author Alberto Ielpo
+ *
+ * This program creates an mqtt sender and a mqtt receiver using the same node process
+ * The goal is to log the QoS 1 flow analysing network congestion and package duplications
+ * Inside the resources folder is defined the config parameters
+ * Inside the log folder will be created the log files
+ *
+ * mqtt-client module contains the mqtt client logic (require mqtt library)
+ * sender contains the publish polling logic
+ * receiver contains the subscription logic
+ */
 const sender = require("./sender.js").main;
 const receiver = require("./receiver.js").main;
 
 try {
     console.log(">> start");
-    // execute in the same thread
     sender();
     receiver();
 } catch (error) {
